f67102f0d0b5e9e3438b2563f70e2a73f7a753cc
unknown <fery@gmail.com>
2019-05-26 10:27:17 -0700

    this is basic

---
+++ resources/views/writer/sidebar.blade.php
@@ -0,0 +1,22 @@
+
+
+<div class="col-md-3">
+            <div class="card">
+                <div class="card-header">
+
+                </div>
+
+                <div class="card-body">
+                    <ul class="nav flex-column" role="tablist">
+
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" href="">
+
+                                </a>
+                            </li>
+
+                    </ul>
+                </div>
+            </div>
+            <br/>
+</div>
